ce9810b3396ec69b760225b6371d49e2960039a3
Prahalad Singh <62698216+Pra101@users.noreply.github.com>
2021-05-04 04:08:27 +0530

    Create Removal_Game.cpp
    This question made me learn a lot of concepts and the very first thing that how not being clear in the beginning makes us waste a lot of time.
    
    There is a list of n numbers and two players who move alternately. On each move, a player removes either the first or last number from the list, and their score increases by that number. Both players try to maximize their scores.
    What is the maximum possible score for the first player when both players play optimally?
    Input
    The first input line contains an integer n: the size of the list.
    The next line has n integers x1,x2,…,xn: the contents of the list.
    Output
    Print the maximum possible score for the first player.

---
+++ Removal_Game.cpp
@@ -0,0 +1,164 @@
+//CSES 
+//Link to this code: https://cses.fi/paste/7bf582202b2f98471fe010/
+// Author: pra101 
+#include <bits/stdc++.h>
+#include<unordered_set>
+#include <cstdio>
+using namespace std;
+#define int long long
+#define float long double
+#define ll long long
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int,int> pii;
+typedef pair<pii,int> ppi;
+typedef vector<pii> vpi;
+typedef priority_queue<ppi> max_heap_ppi;
+typedef priority_queue<int> max_heap_int;
+typedef priority_queue<int, vector<int>, greater<int>> min_heap_int;
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define endl "\n"
+#define d0(x) cout<<(x)<<" "
+#define d1(x) cout<<(x)<<"\n"
+#define d2(x,y) cout<<(x)<<" "<<(y)<<"\n"
+#define d3(x,y,z) cout<<(x)<<" "<<(y)<<" "<<(z)<<"\n"
+#define d4(a,b,c,d) cout<<(a)<<" "<<(b)<<" "<<(c)<<" "<<(d)<<"\n"
+#define fio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define all(v) (v).begin(),(v).end()
+#define sz(v) (v).size()
+//for vector input
+#define enter(a) for (auto &it : (a)) cin >> it
+////////////////////////////////////////////For debugging//////////////////////////////////////////////////
+#define watch(x) cout<<(#x)<<" is "<<(x)<<"\n";
+#define TRACE
+#ifdef TRACE
+    #define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+    template <typename Arg1>
+    void __f(const char* name, Arg1&& arg1){
+        cerr << name << " : " << arg1 << endl;
+    }
+    template <typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names + 1, ',');
+        cerr.write(names, comma - names) << " : " << arg1<<" | ";
+        __f(comma+1, args...);
+    }
+#else
+    #define trace(...) 1
+#endif
+//============================================
+template<class T> ostream& operator<<(ostream &os, vector<T> V) {
+    os << "[ ";
+    for(auto v : V) os << v << " ";
+    return os << "]";
+}
+template<class T> ostream& operator<<(ostream &os, set<T> S){
+    os << "{ ";
+    for(auto s:S) os<<s<<" ";
+    return os<<"}";
+}
+template<class T> ostream& operator<<(ostream &os, unordered_set<T> S){
+    os << "{ ";
+    for(auto s:S) os<<s<<" ";
+    return os<<"}";
+}
+template<class L, class R> ostream& operator<<(ostream &os, pair<L,R> P) {
+    return os << "(" << P.first << "," << P.second << ")";
+}
+template<class L, class R> ostream& operator<<(ostream &os, map<L,R> M) {
+    os << "{ ";
+    for(auto m:M) os<<"("<<m.first<<":"<<m.second<<") ";
+    return os<<"}";
+}
+template<class L, class R> ostream& operator<<(ostream &os, unordered_map<L,R> M) {
+    os << "{ ";
+    for(auto m:M) os<<"("<<m.first<<":"<<m.second<<") ";
+    return os<<"}";
+}
+/*/-----------------------Modular Arithmetic---------------/*/
+const int mod=1e9+7;
+inline int add(int x,int y){
+    x+=y;
+    if (x>=mod) return x-mod;
+    return x;
+}
+inline int sub(int x,int y){
+    x-=y;
+    if (x<0) return x+mod;
+    return x;
+}
+inline int mul(int x,int y){
+    return (x*1ll*y)%mod;
+}
+inline int power(int x,int y){
+    int ans=1;
+    while(y){
+        if (y&1) ans=mul(ans,x);
+        x=mul(x,x);
+        y>>=1;
+    }
+    return ans;
+}
+inline int inv(int x){
+    return power(x,mod-2);
+}
+ //=============LIMITS=====================
+const int sz=5001;
+const int MaxN=1e5+100;
+const int INF=1e16+7;
+ 
+//-----------------------------Code begins----------------------------
+/*
+	Lookout for overflows!!
+	Check array sizes!!
+	Clear before test cases!!
+	Use the correct modulo!!
+	Check for corner cases!!
+	Are you forgetting something?!
+	Read problem statement carefully!!!
+*/
+//-_
+ 
+int dp[sz][sz][2];
+int32_t main(){
+    fio;//Use "flush" to remove from buffer
+    int n,data;
+    cin>>n;
+    vi arr;
+    memset(dp,0,sizeof(dp));
+    for(int i=0;i<n;i++){
+		cin>>data;
+		arr.push_back(data);
+	}
+	
+	for(int i=0;i<n;i++){
+		dp[i][i][1]=0;
+		dp[i][i][0]=arr[i];
+	}
+	//~ //int mx_=0;
+	for(int gap=1;gap<n;gap++){
+		for(int i=0,j=gap;i+gap<n;i++,j++){
+			//~ dp[i][j][0]=max(arr[i]+dp[i+1][j][1],arr[j]+dp[i][j-1][1]);
+			//~ dp[i][j][1]=min(arr[i]+dp[i+1][j][0],arr[j]+dp[i][j-1][0]);
+			
+			if(arr[i]+dp[i+1][j][1]>=arr[j]+dp[i][j-1][1]){
+				dp[i][j][0]=arr[i]+dp[i+1][j][1];// for 1st best score
+				dp[i][j][1]=dp[i+1][j][0]; // for 2nd best score
+			}
+			else {
+				dp[i][j][0]=arr[j]+dp[i][j-1][1]; // for 1st best score
+				dp[i][j][1]=dp[i][j-1][0]; // for 2nd best score
+			}
+		}
+	}
+	//~ for(int i=0;i<=n;i++){
+		//~ for(int j=0;j<=n;j++)cout<<"("<<dp[i][j][0]<<" "<<dp[i][j][1]<<")\t";
+		//~ cout<<"\n";
+	//~ }
+	cout<<dp[0][n-1][0];
+	
+    return 0;
+}
