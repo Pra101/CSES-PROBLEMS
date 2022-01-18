2a1bc4debccf5360529f045b7a91f9374bfabc58
Prahalad Singh <62698216+Pra101@users.noreply.github.com>
2022-01-18 17:12:13 +0530

    Create Range_Xor_Queries.cpp

---
+++ Range_Xor_Queries.cpp
@@ -0,0 +1,17 @@
+//Range Xor Queries
+void Range_Xor_Queries(){
+    int n,q;
+    cin>>n>>q;
+    vector<int> arr(n);
+    enter(arr);
+    int l,r,ans=0;
+    for(int i=1;i<n;i++)arr[i]^=arr[i-1];
+    while(q--){
+	cin>>l>>r;
+	l--,r--;
+	ans=arr[r];
+	if(l>0)ans^=arr[l-1];
+	cout<<ans<<"\n";
+    }
+    return ;
+}
